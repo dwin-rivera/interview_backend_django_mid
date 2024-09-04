7e8848c15e20fafc8c76f68ef27d88de67d810d3
Edwin Rivera <edwinray.rivera@gmail.com>
2024-09-04 12:53:20 -0400

    Challenge 2

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework import generics
 
 from interview.order.models import Order, OrderTag
@@ -13,3 +15,27 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class DeactivateOrderView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def patch(self, request: Request, *args, **kwargs) -> Response:
+        order = self.get_queryset(id=kwargs['id'])
+        serializer = self.serializer_class(order, data=request.data, partial=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+
+        serializer.save()
+
+        return Response(serializer.data, status=200)
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        order = self.get_queryset(id=kwargs['id'])
+        serializer = self.serializer_class(order)
+
+        return Response(serializer.data, status=200)
+
+    def get_queryset(self, **kwargs):
+        return self.queryset.get(**kwargs)

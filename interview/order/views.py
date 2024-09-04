42c0de345afe1b87110e92106e98b3a9bcd0d0f4
Edwin Rivera <edwinray.rivera@gmail.com>
2024-09-04 13:07:21 -0400

    Challenge 3

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework import generics
 
 from interview.order.models import Order, OrderTag
@@ -13,3 +15,16 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class EmbargoDateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get(self, request: Request, start_date: str, embargo_date: str, *args, **kwargs) -> Response:
+        serializer = self.serializer_class(self.get_queryset(start_date, embargo_date), many=True)
+
+        return Response(serializer.data, status=200)
+
+    def get_queryset(self, start_date, embargo_date):
+        return self.queryset.filter(start_date__gt=start_date, embargo_date__lt=embargo_date)
